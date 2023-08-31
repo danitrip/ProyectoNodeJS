fd1b98a932a899d9c7e336ffe12cfb4b69941dac
lucasezc <daniel.tripaylan@gmail.com>
2023-08-30 21:39:39 -0300

    cambios

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 
-//Controllers
-const navigationController = require('./server/controllers/navigationControllers');
-app.get('/',navigationController.getIndex);
+//Middleware
+app.use(express.static(path.join(__dirname, './public')));
 
+//Controllers
+const navigationController = require('./server/controllers/navigationController');
+
+//Routes
+app.get('/',navigationController.getIndex);
+app.get('home', navigationController.getHome);
+
+//Puerto en el que escucha
 app.listen(3000,() => {
     console.log('Server is running on port 3000');
 
---
+++ server/controllers/userController.js
@@ -0,0 +1,9 @@
+const getAll = (req,res) => {
+    res.send('Get all users');
+}
+
+
+const create = (req,res) => {
+    res.send('Create user');
+
+};
