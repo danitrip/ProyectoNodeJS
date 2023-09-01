703ee5adee03845860df76a9bd436d35a05aa37d
Daniel Tripaylan <136761563+danitrip@users.noreply.github.com>
2023-08-31 21:41:40 -0300

    Ejercicio en clase 31/08/23

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
+require('dotenv').config();
 const express = require('express');
-const path = require('path');
+
+const User = require('./db/schemas/UserSchema');
+const userController = require('./controllers/userController');
+const { conectarDB, desconectarDB } = require('./db/conection')
+const PORT = process.env.PORT || 8000;
 const app = express();
 
+app.use(express.json());
 
-//Middleware
-app.use(express.static(path.join(__dirname, './public')));
+app.get('/users', userController.getAll);
+app.post('/user', userController.create);
+app.put("/user/:id", userController.update);
+app.delete("/user/:id", userController.deleteUser);
 
-//Controllers
-const navigationController = require('./server/controllers/navigationController');
-
-const homeController = require('./server/controllers/homeController');
-
-//Routes
-app.get('/',navigationController.getIndex);
-app.get('/home', navigationController.getContacto);
-
-app.get('/api/home',homeController.getContent);
-
-//Puerto en el que escucha
-app.listen(3000,() => {
-    console.log('Server is running on port 3000');
-
+app.listen(PORT, () => {
+    console.log("Servidor corriendo en el puerto " + PORT);
 });
+
+
+// instalar
+// npm i mongoose
+// npm i express
+// npm i dotenv
+// npm i nodemon -D
+
+// Actividad
+// Crear un enpoint GET /products
+app.get('/products', userController.getAll);
+// Crear un enpoint POST /product
+app.post('/product', userController.create);
+// Crear un Schema para Product
+app.put("/product/:id", userController.update);
+// Crear un modelo para Product
